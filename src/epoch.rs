7d92a112792cebef92ff3dfe58b9e43a50634cdc
Roee Shoshani <roysho10@gmail.com>
2026-08-10 23:50:20 +0300

    fix more ordering related todos

--- src/epoch.rs
+++ src/epoch.rs
@@ -10,17 +10,25 @@ pub const MIN_EPOCH_ID: EpochId = 2;
 
 static CUR_EPOCH_ID: Atomic<EpochId> = Atomic::<EpochId>::new(MIN_EPOCH_ID);
 
-/// loads the current epoch id with the given ordering. this only performs a single atomic load operation.
+/// loads the current epoch id atomically with the given ordering. this only performs a single atomic load operation.
 #[inline]
 pub fn epoch_id_get_cur(ordering: atomic::Ordering) -> EpochId {
     CUR_EPOCH_ID.load(ordering)
 }
 
-/// increments the epoch id, returning the new epoch id after the increment.
+/// increments the epoch id atomically, returning the new epoch id after the increment.
+/// the increment has [`Release`](atomic::Ordering::Release) ordering.
 pub fn epoch_id_inc() -> EpochId {
-    // TODO: ordering
-    // TODO: this should probably be `Release`, see `thread_fetch_new_epoch_id_and_update_waiters`.
-    let orig = CUR_EPOCH_ID.fetch_add(2, atomic::Ordering::Relaxed);
+    let orig = CUR_EPOCH_ID.fetch_add(
+        2,
+        // for the load part, we don't need any ordering since we don't need to synchronize with other waiters that increment the epoch
+        // id, we are only synchronizing with users of the rcu protected pointer.
+        //
+        // for the store part, we want release ordering to guarantee that the swapping of the rcu protected pointer happens before
+        // the increment of the epoch id, otherwise someone may see the increment before the swap of the pointer, causing us to release
+        // the data, where he would then try using that freed data.
+        atomic::Ordering::Release,
+    );
 
     if orig != (EpochId::MAX - 1) {
         // SAFETY: in this case, the value is not equal to `EpochId::MAX - 1`, and any value other than
@@ -30,8 +38,11 @@ pub fn epoch_id_inc() -> EpochId {
 
     // in this case, the increment caused the epoch id to become 0.
     // epoch id 0 is reserved, so try incrementing again if we reach 0.
-    // TODO: ordering
-    let orig = CUR_EPOCH_ID.fetch_add(2, atomic::Ordering::Relaxed);
+    let orig = CUR_EPOCH_ID.fetch_add(
+        2,
+        // for an explanation of why we use release ordering here, see the previous epoch id increment operation.
+        atomic::Ordering::Release,
+    );
     if orig == (EpochId::MAX - 1) {
         // in this case, we reached an epoch id of 0 again.
         //
