1603581f131d8d0e5e6d6a50604d0b52694f211b
Roee Shoshani <roysho10@gmail.com>
2026-08-22 06:37:15 -0400

    add a test for no uaf with sleeps

--- tests/main.rs
+++ tests/main.rs
@@ -1,9 +1,10 @@
-use std::sync::Arc;
+use std::{sync::Arc, time::Duration};
 
 use tokio_rcu::{Rcu, TokioRuntimeBuilderExt};
 
+/// a test which makes sure that we don't cause a UAF while stress reading and writing the rcu protected pointer.
 #[test]
-fn no_uaf() {
+fn no_uaf_during_stress() {
     const NUM_READER_TASKS: usize = 64;
     const NUM_WRITER_TASKS: usize = 64;
     const READER_NUM_CLONES: usize = 1000;
@@ -72,6 +73,104 @@ fn no_uaf() {
                             old_str_bytes.fill(b'A');
 
                             tokio::task::yield_now().await;
+                        }
+                    }
+                })
+            })
+            .collect();
+
+        for task in writer_tasks {
+            task.await.unwrap();
+        }
+
+        let _ = data.swap(String::from(final_string)).await;
+
+        for task in reader_tasks {
+            task.await.unwrap();
+        }
+    })
+}
+
+/// a test which makes sure that we don't cause a UAF while stress reading and writing the rcu protected pointer with sleeps
+/// introduced in between.
+/// this is specifically important for checking the race where a waiter sees some worker thread as sleeping, but the worker
+/// thread wakes up immediately and starts using the pointer.
+#[test]
+fn no_uaf_with_sleeps() {
+    const NUM_READER_TASKS: usize = 4;
+    const NUM_WRITER_TASKS: usize = 4;
+    const READER_NUM_CLONES: usize = 1000;
+    const WRITER_NUM_WRITES: usize = 1000;
+    const SHORT_SLEEP_DURATION: Duration = Duration::from_millis(10);
+
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .enable_rcu()
+        .worker_threads(
+            // make sure that we have 1 worker thread per task, so that when a task sleeps the entire thread goes to sleep
+            // instead of scheduling another task.
+            NUM_READER_TASKS + NUM_WRITER_TASKS,
+        )
+        .build()
+        .unwrap();
+
+    rt.block_on(async {
+        let initial_string = "<VALID> initial string";
+        let final_string = "<VALID> final string";
+
+        let data = Arc::new(Rcu::new(String::from(initial_string)));
+        let reader_tasks: Vec<_> = (0..NUM_READER_TASKS)
+            .map(|_| {
+                tokio::spawn({
+                    let data = data.clone();
+                    async move {
+                        loop {
+                            // extra scope to scope the rcu read guard
+                            {
+                                let value = data.read();
+
+                                let orig_value = value.clone();
+
+                                // make sure that the string is one of the valid options.
+                                // the writers overwrite the data of old strings with invalid contents, so that if we happen
+                                // to see any such freed pointer, we will detect the invalid contents and fail the test.
+                                assert!(orig_value.starts_with("<VALID>"));
+
+                                // use the value for a while to try to trigger some UAFs.
+                                for _ in 0..READER_NUM_CLONES {
+                                    let cloned_value = value.clone();
+
+                                    // the same guard should always yield the same data.
+                                    assert_eq!(orig_value, cloned_value);
+                                }
+
+                                if *value == final_string {
+                                    break;
+                                }
+                            }
+
+                            tokio::time::sleep(SHORT_SLEEP_DURATION).await;
+                        }
+                    }
+                })
+            })
+            .collect();
+        let writer_tasks: Vec<_> = (0..NUM_WRITER_TASKS)
+            .map(|writer_id| {
+                tokio::spawn({
+                    let data = data.clone();
+                    async move {
+                        for i in 0..WRITER_NUM_WRITES {
+                            let new_string =
+                                format!("<VALID> hello from worker {} {}", writer_id, i);
+                            let old_str = data.swap(new_string).await;
+
+                            // overwrite the memory of the old string with some invalid data, so that if any reader happens
+                            // to read it, he will detect that it is invalid and fail the test.
+                            let mut old_str_bytes = old_str.into_bytes();
+                            old_str_bytes.fill(b'A');
+
+                            tokio::time::sleep(SHORT_SLEEP_DURATION).await;
                         }
                     }
                 })
