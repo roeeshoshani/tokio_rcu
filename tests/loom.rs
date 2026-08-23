540e980f11f7777fb57c6ce463ed8d64ec658a17
Roee Shoshani <roysho10@gmail.com>
2026-08-23 22:58:47 +0300

    use loom threads with a big stack to fix stack overflow

--- tests/loom.rs
+++ tests/loom.rs
@@ -19,13 +19,36 @@ where
     }
 }
 
+/// spawn a loom thread with a big stack.
+/// our logic uses a lot of stack space, and loom's main thread stack is very small, which leads to a stack overflow.
+/// so, we run most of our heavy logic inside loom threads with big stacks.
+fn loom_spawn<F, T>(f: F) -> loom::thread::JoinHandle<T>
+where
+    F: Send + 'static + FnOnce() -> T,
+    T: Send + 'static,
+{
+    loom::thread::Builder::new()
+        .stack_size(1024 * 1024)
+        .spawn(f)
+        .unwrap()
+}
+
 #[test]
 fn no_uaf_basic() {
     tokio_rcu::loom_tests_api::initialize();
 
     loom::model(|| {
         let state = Arc::new(Rcu::new(String::from("initial")));
-        let worker1 = loom::thread::spawn({
+        let worker1 = loom_spawn({
+            let state = state.clone();
+            move || {
+                tokio_rcu::loom_tests_api::on_thread_start();
+                let prev = busy_block_on_future(state.swap(String::from("new")));
+                assert_eq!(prev, "initial");
+                tokio_rcu::loom_tests_api::on_thread_stop();
+            }
+        });
+        let worker2 = loom_spawn({
             let state = state.clone();
             move || {
                 tokio_rcu::loom_tests_api::on_thread_start();
@@ -35,14 +58,7 @@ fn no_uaf_basic() {
             }
         });
 
-        // main thread is the second worker thread
-        {
-            tokio_rcu::loom_tests_api::on_thread_start();
-            let prev = busy_block_on_future(state.swap(String::from("new")));
-            assert_eq!(prev, "initial");
-            tokio_rcu::loom_tests_api::on_thread_stop();
-        }
-
         worker1.join().unwrap();
+        worker2.join().unwrap();
     })
 }
