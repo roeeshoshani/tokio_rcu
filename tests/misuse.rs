2b032c2acdd45eb5427361c4f59b2e9b1ae92835
Roee Shoshani <roysho10@gmail.com>
2026-09-02 23:25:57 +0300

    more misuse tests

--- tests/misuse.rs
+++ tests/misuse.rs
@@ -1,15 +1,63 @@
-use tokio_rcu::Rcu;
+use std::{any::Any, panic::UnwindSafe, sync::Arc};
+
+use tokio_rcu::{Rcu, rcu_block_on};
+
+const USE_OUTSIDE_OF_RCU_ENABLED_RUNTIME_ERR: &str =
+    "attempted to read an rcu protected pointer outside of an rcu-enabled tokio runtime";
+
+fn extract_string_panic_message(err: Box<dyn Any + Send>) -> String {
+    if let Some(s) = err.downcast_ref::<&str>() {
+        s.to_string()
+    } else if let Some(s) = err.downcast_ref::<String>() {
+        s.clone()
+    } else {
+        panic!("failed to downcast panic message payload")
+    }
+}
+
+fn assert_panics_with_use_outside_of_rcu_enabled_runtime_err<F: FnOnce() + UnwindSafe>(f: F) {
+    let err = std::panic::catch_unwind(move || {
+        f();
+    })
+    .unwrap_err();
+    assert_eq!(
+        extract_string_panic_message(err),
+        USE_OUTSIDE_OF_RCU_ENABLED_RUNTIME_ERR
+    )
+}
 
 #[test]
-#[should_panic = "attempted to read an rcu protected pointer outside of an rcu-enabled tokio runtime"]
 fn read_outside_of_runtime() {
     let x = Rcu::new(Box::new(String::from("some interesting piece of text")));
-    let _ = unsafe { x.read() };
+    assert_panics_with_use_outside_of_rcu_enabled_runtime_err(|| x.with(|_| {}));
 }
 
 #[tokio::test]
-#[should_panic = "attempted to read an rcu protected pointer outside of an rcu-enabled tokio runtime"]
 async fn read_in_non_rcu_runtime() {
     let x = Rcu::new(Box::new(String::from("some interesting piece of text")));
-    let _ = unsafe { x.read() };
+    assert_panics_with_use_outside_of_rcu_enabled_runtime_err(|| x.with(|_| {}));
+}
+
+#[test]
+fn read_from_non_runtime_thread_spawned_inside_runtime() {
+    rcu_block_on(async {
+        let x = Rcu::new(Box::new(String::from("some interesting piece of text")));
+        std::thread::spawn(move || {
+            assert_panics_with_use_outside_of_rcu_enabled_runtime_err(|| x.with(|_| {}));
+        })
+        .join()
+        .unwrap();
+    })
+}
+
+#[test]
+fn read_from_main_thread_after_runtime_finished() {
+    let x = Arc::new(Rcu::new(Box::new(String::from(
+        "some interesting piece of text",
+    ))));
+    rcu_block_on({
+        let x = x.clone();
+        async move { x.with(|_| {}) }
+    });
+    assert_panics_with_use_outside_of_rcu_enabled_runtime_err(|| x.with(|_| {}));
 }
